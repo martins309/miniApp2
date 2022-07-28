4d03b2b4bfe55eb904b42e60f098503f73c25107
Andrew Martins <andrew_omartins@yahoo.com>
2022-07-28 07:58:00 -0400

    .

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,7 @@
 import React from "react";
 
-
 export const HomeScreen = () => {
-    return (
-        <>
-          <h1>This is the HomeScreen my dudes</h1>
-        </>
-    )
+  return (
+    <h1>This is the HomeScreen</h1>
+  )
 }
-
-
